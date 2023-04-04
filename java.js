f91b5baa98a101c8474f5a50d4254056a1a1fbf3
[mohamed-alsayed-2022] <[mohamed-alsayed-2022]>
2023-04-04 22:05:00 +0200

    first

---
+++ java.js
@@ -0,0 +1,119 @@
+
+var lenk =document.getElementById("lenk")
+
+var open=document.getElementById("open")
+var plus=document.getElementById("plus")
+var xmark=document.getElementById("xmark")
+
+
+function bac()  {
+    open.style.display = "block";
+    lenk.style.display = "none";
+    xmark.style.visibility="visible";
+    
+}
+
+function clos(){
+    open.style.display = "none";
+    xmark.style.visibility="hidden";
+    lenk.style.display = "block";
+}
+// end
+
+var lenk1 =document.getElementById("lenk1")
+
+var open1=document.getElementById("open1")
+var plus=document.getElementById("plus")
+var xmark1=document.getElementById("xmark1")
+
+
+function bac1()  {
+    open1.style.display = "block";
+    lenk1.style.display = "none";
+    xmark1.style.visibility="visible";
+    
+}
+
+function clos1(){
+    open1.style.display = "none";
+    xmark1.style.visibility="hidden";
+    lenk1.style.display = "block";
+}
+// end
+var lenk2 =document.getElementById("lenk2")
+
+var open2=document.getElementById("open2")
+var plus=document.getElementById("plus")
+var xmark2=document.getElementById("xmark2")
+
+
+function bac2()  {
+    open2.style.display = "block";
+    lenk2.style.display = "none";
+    xmark2.style.visibility="visible";
+    
+}
+
+function clos2(){
+    open2.style.display = "none";
+    xmark2.style.visibility="hidden";
+    lenk2.style.display = "block";
+}
+// end
+var lenk3 =document.getElementById("lenk3")
+
+var open3=document.getElementById("open3")
+var plus=document.getElementById("plus")
+var xmark3=document.getElementById("xmark3")
+
+
+function bac3()  {
+    open3.style.display = "block";
+    lenk3.style.display = "none";
+    xmark3.style.visibility="visible";
+    
+}
+
+function clos3(){
+    open3.style.display = "none";
+    xmark3.style.visibility="hidden";
+    lenk3.style.display = "block";
+}
+// end
+var lenk4 =document.getElementById("lenk4")
+
+var open4=document.getElementById("open4")
+var xmark4=document.getElementById("xmark4")
+
+
+function bac4()  {
+    open4.style.display = "block";
+    lenk4.style.display = "none";
+    xmark4.style.visibility="visible";
+    
+}
+
+function clos4(){
+    open4.style.display = "none";
+    xmark4.style.visibility="hidden";
+    lenk4.style.display = "block";
+}
+// end
+var lenk5 =document.getElementById("lenk5")
+
+var open5=document.getElementById("open5")
+var xmark5=document.getElementById("xmark5")
+
+
+function bac5()  {
+    open5.style.display = "block";
+    lenk5.style.display = "none";
+    xmark5.style.visibility="visible";
+    
+}
+
+function clos5(){
+    open5.style.display = "none";
+    xmark5.style.visibility="hidden";
+    lenk5.style.display = "block";
+}
